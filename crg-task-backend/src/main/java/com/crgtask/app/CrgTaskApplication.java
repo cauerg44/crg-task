b0448c3999f2a5f258adec5b23be4cb456b8a631
Cauê Garcia <cauerg7@outlook.com>
2025-01-30 14:25:17 -0300

    Project created

---
+++ crg-task-backend/src/main/java/com/crgtask/app/CrgTaskApplication.java
@@ -0,0 +1,13 @@
+package com.crgtask.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrgTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrgTaskApplication.class, args);
+	}
+
+}
