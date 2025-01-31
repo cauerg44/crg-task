2f5a93a7b7e65116b6de716b0f5f5b9cb1e78848
Cauê Garcia <cauerg7@outlook.com>
2025-01-31 17:46:04 -0300

    Task card design

--- crg-task-frontend/src/routes/Home/Task/index.tsx
+++ crg-task-frontend/src/routes/Home/Task/index.tsx
@@ -3,9 +3,48 @@ import './styles.css'
 export default function Task() {
 
     return (
-        <main>
-            <section className='crgtask-container'>
-                <h1>Task page</h1>
+        <main id='crgtask-task-section-page'>
+            <section className='crgtask-container mt45'>
+            <div className='crgtask-task-card'>
+                    <div className='crgtask-line-bottom'>
+                        <h4>Estudar ReactJS</h4>
+                        <p>Aprender sobre Context API e estado global</p>
+                    </div>
+                    <div className='crgtask-task-categories'>
+                        <div className='crgtask-task-category'>
+                            Estudo
+                        </div>
+                        <div className='crgtask-task-category'>
+                            Estudo
+                        </div>
+                        <div className='crgtask-task-category'>
+                            Estudo
+                        </div>
+                        <div className='crgtask-task-category'>
+                            Estudo
+                        </div>
+                    </div>
+                </div>
+                <div className='crgtask-task-card'>
+                    <div className='crgtask-line-bottom'>
+                        <h4>Estudar ReactJS</h4>
+                        <p>Aprender sobre Context API e estado global</p>
+                    </div>
+                    <div className='crgtask-task-categories'>
+                        <div className='crgtask-task-category'>
+                            Estudo
+                        </div>
+                        <div className='crgtask-task-category'>
+                            Estudo
+                        </div>
+                        <div className='crgtask-task-category'>
+                            Estudo
+                        </div>
+                        <div className='crgtask-task-category'>
+                            Estudo
+                        </div>
+                    </div>
+                </div>
             </section>
         </main>
     )
