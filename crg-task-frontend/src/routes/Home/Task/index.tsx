490494611110be1b3f00ac7cd3a3405bf609f135
Cauê Garcia <cauerg7@outlook.com>
2025-02-04 17:13:47 -0300

    Button to navigate to create new task - frontend

--- crg-task-frontend/src/routes/Home/Task/index.tsx
+++ crg-task-frontend/src/routes/Home/Task/index.tsx
@@ -1,6 +1,8 @@
 import './styles.css';
 import * as taskService from '../../../services/task-service.ts';
 import TaskCard from '../../../components/TaskCard/index.tsx';
+import ButtonPrimary from '../../../components/ButtonPrimary/index.tsx';
+import { Link } from 'react-router-dom';
 
 export default function Task() {
     const tasks = taskService.findAll();
@@ -8,13 +10,19 @@ export default function Task() {
     return (
         <main id='crgtask-task-section-page'>
             <section className='crgtask-container mt45'>
+                <div className='crgtask-navigate-create-new-task'>
+                    <h2>Gostaria de adicionar uma tarefa? 📝</h2>
+                    <Link to="/create/task">
+                        <ButtonPrimary text='Clique aqui' />
+                    </Link>
+                </div>
                 {tasks.length > 0 ? (
                     tasks.map(task => <TaskCard key={task.id} task={task} />)
-                ) 
-                : 
-                (
-                    <h1>Não há tarefas cadastradas!</h1>
-                )}
+                )
+                    :
+                    (
+                        <h1>Não há tarefas cadastradas!</h1>
+                    )}
             </section>
         </main>
     );
