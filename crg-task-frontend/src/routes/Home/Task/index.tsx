61d7b47bc33d462ea4e4276ce11f2e721097b5f8
Cauê Garcia <cauerg7@outlook.com>
2025-02-04 17:02:10 -0300

    Condicional rendering tasks

--- crg-task-frontend/src/routes/Home/Task/index.tsx
+++ crg-task-frontend/src/routes/Home/Task/index.tsx
@@ -1,17 +1,21 @@
-import './styles.css'
-import * as taskService from '../../../services/task-service.ts'
-import TaskCard from '../../../components/TaskCard/index.tsx'
+import './styles.css';
+import * as taskService from '../../../services/task-service.ts';
+import TaskCard from '../../../components/TaskCard/index.tsx';
 
 export default function Task() {
+    const tasks = taskService.findAll();
 
     return (
         <main id='crgtask-task-section-page'>
             <section className='crgtask-container mt45'>
-                {
-                    taskService.findAll().map(
-                        task => <TaskCard key={task.id} task={task} />)
-                }
+                {tasks.length > 0 ? (
+                    tasks.map(task => <TaskCard key={task.id} task={task} />)
+                ) 
+                : 
+                (
+                    <h1>Não há tarefas cadastradas!</h1>
+                )}
             </section>
         </main>
-    )
+    );
 }
